825bdc2762acd2c92c415a32b88a5320e0933b5e
Jonathan Cheng <jcheng461@gmail.com>
2024-01-07 13:35:28 -0800

    update to randomly select user selected number results to display

--- src/components/Control.js
+++ src/components/Control.js
@@ -13,6 +13,7 @@ function Control() {
   // useState controls state
   // const [formVisible, setFormVisible] = useState(false);
   const [userQuery, setUserQuery] = useState(null);
+  const [numPairsSelected, setNumPairsSelected] = useState(null);
   const [resultsToUI, setResultsToUI] = useState(null);
 
   // Functions
@@ -25,28 +26,33 @@ function Control() {
   // Populate variable userQuery with user form results
   function collectValuesFromForm(firstValue, secondValue, numberOfPairs) {
     concatenateUserInput(firstValue, secondValue);
+    setNumPairsSelected(numberOfPairs);
   }
 
-  async function queryFirebaseDocs() {
+  // WIP: Allow user to choose "consonant-pairs" or "vowels-diphthongs"
+
+  async function gatherAndFilterResults() {
     if (userQuery !== null) {
       console.log("queryFirebaseDocs triggered, userQuery: ", userQuery);
       const docRef = doc(db, "consonant-pairs", userQuery);
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log("Document data: ", docSnap.data());
-        console.log("Document num Fields: ", Object.keys(docSnap.data()).length);
-        setResultsToUI(docSnap.data());
+        // Before setting results to UI, randomly select and filter them.
+        const arrayOfSelectedIds = randomPairPicker(Object.keys(docSnap.data()).length, numPairsSelected);
+        console.log("arrayOfSelectedIds: ", arrayOfSelectedIds);
+        console.log("gatherAndFilterResults, docSnap.data(): ", docSnap.data());
+        filterDocResults(docSnap.data(), arrayOfSelectedIds);
       } else {
         console.log("No such document. Reversing query!");
         const reversedQuery = userQuery.split("").reduce((accumulator, char) => char + accumulator, "");
-        console.log("reversedQuery is: ", reversedQuery);
         const docRef = doc(db, "consonant-pairs", reversedQuery);
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
-          console.log("Document data: ", docSnap.data());
-          console.log("Document num Fields: ", Object.keys(docSnap.data()).length);
-          setResultsToUI(docSnap.data());
+          const arrayOfSelectedIds = randomPairPicker(Object.keys(docSnap.data()).length, numPairsSelected);
+          console.log("arrayOfSelectedIds: ", arrayOfSelectedIds);
+          console.log("gatherAndFilterResults, docSnap.data(): ", docSnap.data());
+          filterDocResults(docSnap.data(), arrayOfSelectedIds);
         } else {
           console.log("No such document!");
         }
@@ -58,8 +64,43 @@ function Control() {
   // Randomly select X number of Ids, where X is "numberOfPairs" chosen by user.
   // send to the Results page these pairs.
 
+  // WIP: Randomly choose pairs
+  // Consider the queried document, i.e.LR, take the number of pairs
+  // Object.keys(docSnap.data()).length);
+  // Randomly select a quantity of pairs, chosen by user -- "numberOfPairs"
+  // Send these pairs to the results
+
+  function randomPairPicker(numPairsAvailable, numPairsToSelect) {
+    // Pass in numbers of pairs wanted (numPairsToSelect).
+    // Pass in numbers of pairs available in that document (numPairsAvailable)
+    let randomlySelectedIds = new Set(),
+      ans;
+    while (randomlySelectedIds.size < numPairsToSelect) {
+      randomlySelectedIds.add(Math.floor(Math.random() * numPairsAvailable));
+    }
+    ans = [...randomlySelectedIds];
+    console.log("randomPairsPicker: ", ans);
+    // Save these into an array. Display to console.
+    return ans;
+  }
+
+  // Now that we have an array of random Ids..
+  // Use this to filter the document
+  function filterDocResults(document, arrayOfSelectedIds) {
+    console.log("filterDocResults, document: ", document);
+    const convertedArray = arrayOfSelectedIds.map(String);
+    console.log("filterDocResults, convertedArray: ", convertedArray);
+    const docResultsToArray = Object.entries(document);
+    console.log("filterDocResults, docResultsToArray: ", docResultsToArray);
+    const filteredResults = docResultsToArray.filter((entry) => convertedArray.includes(entry["0"]));
+    console.log("filterDocResults, filteredResults: ", filteredResults);
+    const convertedBackToObj = Object.fromEntries(filteredResults);
+    console.log("filterDocResults, convertedBackToObj: ", convertedBackToObj);
+    setResultsToUI(convertedBackToObj);
+  }
+
   useEffect(() => {
-    queryFirebaseDocs();
+    gatherAndFilterResults();
   }, [userQuery]);
 
   let currentlyVisiblePage = null;
@@ -132,3 +173,32 @@ export default Control;
 //   setPairResults(results);
 //   return () => unSubscribe();
 // }, []);
+
+// const documentExample = {
+//   0: {
+//     pair: ["biking", "Viking"],
+//     id: "0",
+//   },
+//   1: {
+//     pair: ["knobble", "novel"],
+//     id: "1",
+//   },
+//   2: {
+//     id: "2",
+//     pair: ["bat", "vat"],
+//   },
+//   3: {
+//     pair: ["back", "vac"],
+//     id: "3",
+//   },
+//   4: {
+//     pair: ["ballet", "valet"],
+//     id: "4",
+//   },
+// };
+
+// console.log("documentExample[0]: ", documentExample[0]);
+// console.log("documentExample[0].id: ", documentExample[0].id);
+// console.log("documentExample[0].pair: ", documentExample[0].pair);
+// console.log("documentExample[0].pair[0]: ", documentExample[0].pair[0]);
+// console.log("documentExample[0].pair[1]: ", documentExample[0].pair[1]);
