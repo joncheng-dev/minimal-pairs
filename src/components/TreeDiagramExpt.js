3ffad5a4c370b4a3a51bd7a85ea79372b4eb6a17
Jon Cheng <joncheng.dev@gmail.com>
2024-01-28 22:47:12 -0800

    WIP: dynamic tree rendering; refactor to use one useState for 2nd row children

--- src/components/TreeDiagramExpt.js
+++ src/components/TreeDiagramExpt.js
@@ -8,31 +8,75 @@ const containerStyles = {
   height: "50vh",
 };
 
-const drawTree = (title) => {
+const drawTop = (title, wordOne, wordTwo) => {
   return {
     name: title,
-    children: [{ name: "firstWord" }, { name: "secondWord" }],
+    children: [{ name: wordOne }, { name: wordTwo }],
   };
+};
+
+const drawTopWhenSecondRowPresent = (title, wordOne, wordTwo, children) => {
+  console.log("TreeDiagramExpt, drawTopWhenSecondRowPresent, children[0]: ", children[0]);
+  console.log("TreeDiagramExpt, drawTopWhenSecondRowPresent, children[1]: ", children[1]);
+  // prettier-ignore
+  return {
+    name: title,
+    children: [{ name: wordOne, children: children[0] }, { name: wordTwo, children: children[1] }],
+  };
+};
+
+// const drawSecondRow = (firstWord, secondWord) => {
+//   return {
+//     children: [{ name: firstWord }, { name: secondWord }],
+//   };
+// };
+const drawSecondRow = (firstWord, secondWord) => {
+  return [{ name: firstWord }, { name: secondWord }];
 };
 
 function TreeDiagramExpt(props) {
   const { results, treeDiagramName, userQuery } = props;
-
-  const resultsToArray = Object.entries(results);
-  console.log("TreeDiagramExpt, resultsToArray: ", resultsToArray);
-  const data = drawTree(treeDiagramName);
+  console.log("TreeDiagramExpt, results: ", results);
   const [rowOne, setRowOne] = useState([]);
   const [rowTwo, setRowTwo] = useState([]);
+  const [children, setChildren] = useState([]);
+  const [data, setData] = useState({});
 
-  // useEffect(() => {
-  //   arrayToRows();
-  // }, []);
+  useEffect(() => {
+    arrayToRows();
+  }, []);
 
-  // function arrayToRows(resultsToArray) {
-  //   if (resultsToArray.length === 2) {
-  //     setRowOne(resultsToArray);
-  //   }
-  // }
+  useEffect(() => {
+    // setData(drawTop(treeDiagramName, rowOne[0], rowOne[1]));
+    setChildren([drawSecondRow(rowTwo[0], rowTwo[1]), drawSecondRow(rowTwo[2], rowTwo[3])]);
+  }, [rowOne]);
+
+  useEffect(() => {
+    console.log("TreeDiagramExpt, children: ", children);
+    setData(drawTopWhenSecondRowPresent(treeDiagramName, rowOne[0], rowOne[1], children));
+  }, [children]);
+
+  useEffect(() => {
+    // console.log("TreeDiagramExpt, children2: ", children2);
+    console.log("TreeDiagramExpt, data: ", data);
+  }, [data]);
+
+  function arrayToRows() {
+    if (Object.keys(results).length === 1) {
+      console.log("TreeDiagramExpt, results, length: ", Object.keys(results).length);
+      const flattenedArray = Object.values(results).flat();
+      console.log("TreeDiagramExpt, arrayToRows, flattenedArray: ", flattenedArray);
+      setRowOne(flattenedArray.slice(0, 2));
+    }
+    if (Object.keys(results).length === 3) {
+      const flattenedArray = Object.values(results).flat();
+      console.log("TreeDiagramExpt, results, length 3: ", Object.keys(results).length);
+      setRowOne(flattenedArray.slice(0, 2));
+      console.log("TreeDiagramExpt, arrayToRows, flattenedArray: ", flattenedArray);
+      console.log("flattenedArray.slice(2,5), aka rowTwo: ", flattenedArray.slice(2, 6));
+      setRowTwo(flattenedArray.slice(2, 6));
+    }
+  }
 
   //#endregion Data Allocation
 
